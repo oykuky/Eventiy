3d89b126cdb9734bd6a803d794fc4e3fb5c52135
Öykü Kaya <oykuky001@gmail.com>
2025-03-02 22:38:28 +0300

    Improve: Scroll effect for Navbar and styling updates for mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,24 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { AlignJustify } from "lucide-react";
 
-
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const navItems = [
     { name: "Ana Sayfa", href: "/" },
@@ -15,40 +28,50 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="sticky top-0 bg-white/70 shadow-lg">
+    <nav
+      className={`sticky top-0 transition-all duration-300 ${
+        isScrolled ? "bg-black/5 backdrop-blur-md shadow-lg" : "bg-transparent"
+      }`}
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20 p-2">
           {/* Logo */}
-          <div className="flex items-center px-5 shadow-green-300 bg-green-100/5 hover:bg-green-100 transation-all duration-300 ease-in shadow-lg rounded-xl cursor-pointer">
-              <Link
-                href="/"
-                className="font-display text-black/80 text-2xl font-bold text-primary-600"
-              >
-                Eventiy
-              </Link>
+          <div className="flex items-center px-3 cursor-pointer">
+            <Link
+              href="/"
+              className="font-display text-black/80 text-2xl font-bold"
+            >
+              Eventiy
+            </Link>
           </div>
 
           {/* Menu */}
           <div className="hidden md:flex md:items-center md:space-x-8">
             {navItems.map((i, index) => (
-              <Link className="active:bg-amber-200/30 p-1.5 rounded-lg" key={index} href={i.href}>
+              <Link
+                className="active:bg-slate-200 hover:text-emerald-500 p-1.5 rounded-lg"
+                key={index}
+                href={i.href}
+              >
                 {i.name}
               </Link>
             ))}
-            <div className="flex items-center space-x-4">
-              <Link
-                href="/login"
-                className="border-green-400/50 border-2 border-b-4 active:border-b-2 rounded-lg text-primary-600 hover:text-primary-700 px-4 py-2 font-medium"
-              >
-                Giriş Yap
-              </Link>
-              <Link
-                href="/register"
-                className="border-green-400/50 border-2 border-b-4 active:border-b-2 rounded-lg text-primary-600 hover:text-primary-700 px-4 py-2 font-medium"
-              >
-                Kayıt Ol
-              </Link>
-            </div>
+          </div>
+
+          {/* Buttons */}
+          <div className="hidden md:flex md:items-center md:space-x-4">
+            <Link
+              href="/login"
+              className="border-green-400/50 border-2 border-b-4 active:border-b-2 rounded-lg text-primary-600 hover:text-primary-700 px-4 py-2 font-medium"
+            >
+              Giriş Yap
+            </Link>
+            <Link
+              href="/register"
+              className="border-green-400/50 border-2 border-b-4 active:border-b-2 rounded-lg text-primary-600 hover:text-primary-700 px-4 py-2 font-medium"
+            >
+              Kayıt Ol
+            </Link>
           </div>
 
           {/* Mobile menu*/}
@@ -65,22 +88,26 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="md:hidden">
-          <div className="flex flex-col items-center p-2 py-3 space-y-1">
+        <div className="md:hidden px-4">
+          <div className="bg-slate-200/10 flex flex-col items-start p-2 py-3 space-y-1 gap-3 rounded-lg mt-5">
             {navItems.map((i, index) => (
-              <Link key={index} href={i.href}>
+              <Link
+                className="hover:text-emerald-500"
+                key={index}
+                href={i.href}
+              >
                 {i.name}
               </Link>
             ))}
             <Link
               href="/login"
-              className="block px-3 py-2 text-primary-600 hover:text-primary-600"
+              className="mt-4 bg-black text-white hover:bg-emerald-500 transation-all duration-300 ease-in rounded-lg text-primary-600 hover:text-primary-700 px-4 py-2 font-medium"
             >
               Giriş Yap
             </Link>
             <Link
               href="/register"
-              className="block px-3 py-2 text-primary-600 hover:text-primary-700"
+              className="bg-black text-white hover:bg-emerald-500 transation-all duration-300 ease-in rounded-lg text-primary-600 hover:text-primary-700 px-4 py-2 font-medium"
             >
               Kayıt Ol
             </Link>
