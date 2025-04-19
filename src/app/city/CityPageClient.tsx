76b4f4608852b855c00382fa488b9c8ba2022f4f
Öykü Kaya <oykuky001@gmail.com>
2025-04-19 21:26:38 +0300

    fix

---
+++ src/app/city/CityPageClient.tsx
@@ -0,0 +1,48 @@
+"use client"
+import EventCard from "@/components/EventCard";
+import { useEvents } from "@/hooks/queries/useEvents";
+import { Loader } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+
+function CityPage() {
+  const searchParams = useSearchParams();
+  const cityId = searchParams.get("cityId");
+  const { data, isLoading, error } = useEvents(Number(cityId));
+
+  if (isLoading)
+    return (
+      <div className="mt-10 flex items-center justify-center">
+        <Loader className="text-center text-green-700 animate-spin w-12 h-12" />
+      </div>
+    );
+
+    if (error)
+      return (
+        <div className="mt-10 flex items-center justify-center">
+          <p className="text-center">Hata oluştu!</p>
+        </div>
+      );
+  
+    if (!data?.items?.length)
+      return (
+        <div className="mt-10 flex items-center justify-center">
+          <p className="text-center">Etkinlik bulunamadı.</p>
+        </div>
+      );
+  
+
+  return (
+    <div className="flex items-center justify-center flex-col gap-8">
+      <div className="container mx-auto px-6 lg:px-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          {data.items.map((event) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CityPage;
--- src/app/format/page.tsx
+++ src/app/format/page.tsx
@@ -1,48 +1,11 @@
-"use client"
-import EventCard from '@/components/EventCard';
-import { useEvents } from '@/hooks/queries/useEvents'
-import { Loader } from 'lucide-react';
-import { useSearchParams } from 'next/navigation'
-import React from 'react'
+import { Suspense } from "react";
+import FormatPage from "./FormatPage";
 
-function FormatPage() {
-    const params = useSearchParams();
-    const formatId = params.get('formatId')
-    const { data, isLoading, error} = useEvents(Number(formatId))
-    
-    if (isLoading)
-        return (
-          <div className="mt-10 flex items-center justify-center">
-            <Loader className="text-center text-green-700 animate-spin w-12 h-12" />
-          </div>
-        );
-        
-        if (error)
-          return (
-            <div className="mt-10 flex items-center justify-center">
-              <p className="text-center">Hata oluştu!</p>
-            </div>
-          );
-      
-        if (!data?.items?.length)
-          return (
-            <div className="mt-10 flex items-center justify-center">
-              <p className="text-center">Etkinlik bulunamadı.</p>
-            </div>
-          );
-      
-    
-      return (
-        <div className="flex items-center justify-center flex-col gap-8">
-          <div className="container mx-auto px-6 lg:px-8">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {data.items.map((event) => (
-                <EventCard key={event.id} event={event} />
-              ))}
-            </div>
-          </div>
-        </div>
-      );
+
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <FormatPage/>
+    </Suspense>
+  )
 }
-
-export default FormatPage
